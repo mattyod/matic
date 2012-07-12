06c3cc379cb12a828251a2bcff9725cf4b5212e6
Matt O'Donoghue <mattyod@gmail.com>
2012-07-12 18:17:44 +0100

    moved closing output into bin file. Tidied help

--- bin/args/help.js
+++ bin/args/help.js
@@ -11,16 +11,7 @@
 var fs = require('fs');
 
 module.exports = (function() {
-  var help = 
-  '\n' +
-  'Schema.js\n' +
-  '---------\n\n' +
-  'Arguments\n' +
-  '---------\n' +
-  '  --help, -h, help: All the helps.' +
-  '\n\n';
-
-  console.log(__dirname);
+  
   var help = fs.readFileSync(__dirname + '/output/help.txt', 'binary');
 
   // Write out help text to the terminal

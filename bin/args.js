c8f26289273f27496bbbf29ea52052b23fb9b4ec
Matt O'Donoghue <mattyod@gmail.com>
2012-07-13 16:23:59 +0100

    spaces v tabs

--- bin/args.js
+++ bin/args.js
@@ -1,34 +1,34 @@
 /*
-	
-	args.js
 
-	Check for any arguments passed with the call to schema and
-	route them accordingly
-	
+  args.js
+
+  Check for any arguments passed with the call to schema and
+  route them accordingly
+
 */
 
 module.exports = (function() {
 
-	// Capture any arguments passed to shcema
-	var args = process.argv.slice(2);
+  // Capture any arguments passed to shcema
+  var args = process.argv.slice(2);
 
-	if(args.length) {
-		switch(args[0]) {
-			case '--help':
-			case '-help':
-			case '-h':
-			case 'help': 
-				require('./args/help');
-				break;
-			case '--version':
-			case '-version':
-			case '-v':
-			case 'version':
-				require('./args/version');
-				break;
-			default:
-				process.stdout.write('Unknown argument, try schema help');
-		}
-	}
+  if(args.length) {
+    switch(args[0]) {
+      case '--help':
+      case '-help':
+      case '-h':
+      case 'help': 
+        require('./args/help');
+        break;
+      case '--version':
+      case '-version':
+      case '-v':
+      case 'version':
+        require('./args/version');
+        break;
+      default:
+        process.stdout.write('Unknown argument, try schema help');
+    }
+  }
 
 })();

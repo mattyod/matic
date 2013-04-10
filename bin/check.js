d46e72865c849b964dd04433e08b9b6b14f64e46
Matthew O'Donoghue <mattyod@gmail.com>
2013-04-10 15:26:29 +0100

    graceful failure for missing template engine

---
+++ bin/check.js
@@ -0,0 +1,30 @@
+/*
+    check.js
+
+    Perform a check to see if the specified template engine is available.
+    If it isn't offer a friendly error message
+
+*/
+
+module.exports = function (config) {
+
+  var msgs = {
+        configError: "The config file does not specify a templating engine, please add one.\n",
+        requireError: ""
+      };
+
+  if (config.template && config.template.lib) {
+
+    msgs.requireError = "The config file speicifies " + config.template.lib + 
+    " as the templating engine but it is not installed. \nPlease run: \n" +
+    "npm install -g " + config.template.lib + "\n";
+
+    try {
+      require.resolve(config.template.lib);
+    } catch (error) {
+      process.stdout.write(msgs.requireError);
+    }
+  } else {
+    process.stdout.write(msgs.configError)
+  }
+};
